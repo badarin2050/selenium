2703ee419922f21ec8ffd1dabe1a419ecd38e8a6
Haitham Badarin <31930846+badarin2050@users.noreply.github.com>
2019-11-23 00:07:41 +0200

    pycharm

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
+
 from selenium import webdriver
 browser = webdriver.Chrome('/Users/user/Downloads/chromedriver')
+browser.get('https://www.eschool.edu.ps/index.php?module=students&page=ListView')
+
